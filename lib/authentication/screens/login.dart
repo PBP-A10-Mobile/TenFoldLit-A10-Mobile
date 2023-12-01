65ea0f306fc0f2dead1065ffa7bdb32a5e048728
nabielsubhan <mnsubhan1509@gmail.com>
2023-12-01 09:59:45 +0700

    minor changes

--- lib/authentication/screens/login.dart
+++ lib/authentication/screens/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
-import 'package:tenfoldlit_mobile/authentication/register.dart';
+import 'package:tenfoldlit_mobile/authentication/screens/register.dart';
 import 'package:tenfoldlit_mobile/homepage/screens/menu.dart';
 
 void main() {
