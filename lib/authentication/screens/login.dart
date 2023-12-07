9d32c69fd9e0284fc063109af0990be8fe83428b
nabielsubhan <mnsubhan1509@gmail.com>
2023-12-07 22:26:33 +0700

    added designed to login page and register page

--- lib/authentication/screens/login.dart
+++ lib/authentication/screens/login.dart
@@ -33,93 +33,178 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
     final TextEditingController _usernameController = TextEditingController();
     final TextEditingController _passwordController = TextEditingController();
+    bool passToggle = false;
 
     @override
     Widget build(BuildContext context) {
         final request = context.watch<CookieRequest>();
         return Scaffold(
+          backgroundColor: Colors.brown.shade50,
             appBar: AppBar(
-                title: const Text('Login'),
+                title: Center(child: const Text('TenfoldLit')),
+                backgroundColor: Colors.brown.shade400,
+                foregroundColor: Colors.white,
             ),
-            body: Container(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                        TextField(
-                            controller: _usernameController,
-                            decoration: const InputDecoration(
-                                labelText: 'Username',
+            body: SafeArea(
+              child: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Center(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                            const Text(
+                              'Login',
+                              style: TextStyle(
+                                fontSize: 20
+                              ),
                             ),
-                        ),
-                        const SizedBox(height: 12.0),
-                        TextField(
-                            controller: _passwordController,
-                            decoration: const InputDecoration(
-                                labelText: 'Password',
+                            const SizedBox(height: 30,),
+                            TextField(
+                                controller: _usernameController,
+                                cursorColor: Colors.black,
+                                decoration: InputDecoration(
+                                    labelText: 'Username',
+                                    hintText: 'Username',
+                                    hintStyle: const TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 14
+                                    ),
+                                    labelStyle: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400
+                                    ),
+                                    prefixIcon: const Icon(Icons.account_circle_outlined, color: Colors.black, size: 18,),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.grey.shade400, width: 2),
+                                      borderRadius: BorderRadius.circular(10)
+                                    ),
+                                    floatingLabelStyle: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 18
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.black, width: 1.5),
+                                      borderRadius: BorderRadius.circular(10)
+                                    )
+                                ),
                             ),
-                            obscureText: true,
-                        ),
-                        const SizedBox(height: 24.0),
-                        ElevatedButton(
-                            onPressed: () async {
-                                String username = _usernameController.text;
-                                String password = _passwordController.text;
+                            const SizedBox(height: 12.0),
+                            TextField(
+                                controller: _passwordController,
+                                cursorColor: Colors.black,
+                                decoration: InputDecoration(
+                                    labelText: 'Password',
+                                    hintText: 'Password',
+                                    hintStyle: const TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 14
+                                    ),
+                                    labelStyle: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400
+                                    ),
+                                    prefixIcon: const Icon(Icons.key_rounded, color: Colors.black, size: 18,),
+                                    suffixIcon: InkWell(
+                                      onTap: () {
+                                        setState(() {
+                                          passToggle = !passToggle;
+                                        });
+                                      },
+                                      child: Icon(passToggle ? Icons.visibility : Icons.visibility_off),
+                                    ),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.grey.shade400, width: 2),
+                                      borderRadius: BorderRadius.circular(10)
+                                    ),
+                                    floatingLabelStyle: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 18
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.black, width: 1.5),
+                                      borderRadius: BorderRadius.circular(10)
+                                    )
+                                ),
+                                obscureText: !passToggle,
+                            ),
+                            const SizedBox(height: 30.0),
+                            MaterialButton(
+                                onPressed: () async {
+                                    String username = _usernameController.text;
+                                    String password = _passwordController.text;
+                              
+                                    // Cek kredensial
+                                    // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
+                                    // Untuk menyambungkan Android emulator dengan Django pada localhost,
+                                    // gunakan URL http://10.0.2.2/
+                                    final response = await request.login("http://127.0.0.1:8000/login_flutter/", {
+                                    'username': username,
+                                    'password': password,
+                                    });
+                        
+                                    if (request.loggedIn) {
+                                        String message = response['message'];
+                                        String uname = response['username'];
+                                        Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(builder: (context) => HomePage()),
+                                        );
+                                        ScaffoldMessenger.of(context)
+                                            ..hideCurrentSnackBar()
+                                            ..showSnackBar(
+                                                SnackBar(content: Text("$message Selamat datang, $uname.")));
+                                        } else {
+                                        showDialog(
+                                            context: context,
+                                            builder: (context) => AlertDialog(
+                                                title: const Text('Login Gagal'),
+                                                content:
+                                                    Text(response['message']),
+                                                actions: [
+                                                    TextButton(
+                                                        child: const Text('OK'),
+                                                        onPressed: () {
+                                                            Navigator.pop(context);
+                                                        },
+                                                    ),
+                                                ],
+                                            ),
+                                        );
+                                    }
+                                },
+                                height: 45,
+                                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+                                shape: RoundedRectangleBorder(
 
-                                // Cek kredensial
-                                // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
-                                // Untuk menyambungkan Android emulator dengan Django pada localhost,
-                                // gunakan URL http://10.0.2.2/
-                                final response = await request.login("http://127.0.0.1:8000/login_flutter/", {
-                                'username': username,
-                                'password': password,
-                                });
-                    
-                                if (request.loggedIn) {
-                                    String message = response['message'];
-                                    String uname = response['username'];
+                                  borderRadius: BorderRadius.circular(10)
+                                ),
+                                color: Colors.brown,
+                                child: const Text('Login', style: TextStyle(color: Colors.white),),
+                            ),
+                            const SizedBox(height: 15.0),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text("Don't have an account yet?", style: TextStyle(color: Colors.grey.shade600, fontSize: 14),),
+                                TextButton(
+                                  onPressed: () {
                                     Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(builder: (context) => HomePage()),
+                                      context,
+                                      MaterialPageRoute(builder: (context) => RegisterPage()),
                                     );
-                                    ScaffoldMessenger.of(context)
-                                        ..hideCurrentSnackBar()
-                                        ..showSnackBar(
-                                            SnackBar(content: Text("$message Selamat datang, $uname.")));
-                                    } else {
-                                    showDialog(
-                                        context: context,
-                                        builder: (context) => AlertDialog(
-                                            title: const Text('Login Gagal'),
-                                            content:
-                                                Text(response['message']),
-                                            actions: [
-                                                TextButton(
-                                                    child: const Text('OK'),
-                                                    onPressed: () {
-                                                        Navigator.pop(context);
-                                                    },
-                                                ),
-                                            ],
-                                        ),
-                                    );
-                                }
-                            },
-                            child: const Text('Login'),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(builder: (context) => RegisterPage()),
-                            );
-                          },
-                          child: const Text(
-                            "Don't have an account yet? Register here",
-                          ),
-                        ),
-                    ],
-                ),
+                                  },
+                                  child: const Text(
+                                    "Register here",
+                                  ),
+                                ),
+                              ],
+                            ),
+                        ],
+                    ),
+                  ),
+              ),
             ),
         );
     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
-import 'package:tenfoldlit_mobile/authentication/login.dart';
+import 'package:tenfoldlit_mobile/authentication/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
