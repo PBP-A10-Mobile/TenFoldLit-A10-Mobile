ef2823e775aecd9f6792c413392cc9f0cb98a512
nabielsubhan <mnsubhan1509@gmail.com>
2023-12-07 10:27:00 +0700

    added user model

--- lib/friends/screens/friends.dart
+++ lib/friends/screens/friends.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 import 'dart:convert';
 import 'package:tenfoldlit_mobile/friends/models/UserConnections.dart';
+import 'package:tenfoldlit_mobile/friends/models/user.dart';
 import 'package:tenfoldlit_mobile/homepage/widgets/left_drawer.dart';
+import 'dart:developer';
 
 class FriendsPage extends StatefulWidget {
     const FriendsPage({Key? key}) : super(key: key);
@@ -13,29 +17,36 @@ class FriendsPage extends StatefulWidget {
 
 class _FriendsPageState extends State<FriendsPage> {
 Future<List<UserConnections>> fetchFriends() async {
-    // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
-    var url = Uri.parse(
-        'http://127.0.0.1:8000/get_friends/');
-    var response = await http.get(
-        url,
-        headers: {"Content-Type": "application/json"},
-    );
+  final request = context.watch<CookieRequest>();
+  var url = 'http://127.0.0.1:8000/get_friends/';
+  var response = await request.get(url);
 
-    // melakukan decode response menjadi bentuk json
-    var dataUser = jsonDecode(utf8.decode(response.bodyBytes));
 
-    // melakukan konversi data json menjadi object Product
-    List<UserConnections> friends_list = [];
-    for (var friend in dataUser[0].fields.friends) {
-        if (friend != null) {
-            friends_list.add(UserConnections.fromJson(friend));
+  // melakukan konversi data json menjadi object Item
+    List<UserConnections> list_userconnections = [];
+    for (var d in response) {
+        if (d != null) {
+            list_userconnections.add(UserConnections.fromJson(d));
         }
     }
-    return friends_list;
+    return list_userconnections;
+}
+
+Future<List<User>> fetchUser(int userId) async {
+  final request = context.watch<CookieRequest>();
+  var url = 'http://127.0.0.1:8000/get_user/$userId';
+  var response = await request.get(url);
+  
+  List<User> user_data = [];
+  for (var d in response) {
+    user_data.add(User.fromJson(d));
+  }
+  return user_data;
 }
 
 @override
 Widget build(BuildContext context) {
+  final request = context.watch<CookieRequest>();
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.brown,
@@ -51,9 +62,6 @@ Widget build(BuildContext context) {
           padding: const EdgeInsets.all(10),
           child: Column(
             children: [
-              // const SizedBox(
-              //   height: 20,
-              // ),
               const TextField(
                 decoration: InputDecoration(
                   labelText: 'Search', suffixIcon: Icon(Icons.search)
@@ -62,6 +70,56 @@ Widget build(BuildContext context) {
               const SizedBox(
                 height: 20,
               ),
+              FutureBuilder(
+                future: fetchFriends(),
+                builder: (context, AsyncSnapshot snapshot) {
+                    if (snapshot.data == null) {
+                        return const Center(child: CircularProgressIndicator());
+                    } else {
+                        if (!snapshot.hasData) {
+                        return const Column(
+                            children: [
+                            Text(
+                                "Anda belum mempunyai teman.",
+                                style:
+                                    TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                            ),
+                            SizedBox(height: 8),
+                            ],
+                        );
+                    } else {
+                        return ListView.builder(
+                          shrinkWrap: true,
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (_, index) => Container(
+                              margin: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 12),
+                              padding: const EdgeInsets.all(20.0),
+                              child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                FutureBuilder(
+                                  future: fetchUser(snapshot.data![index].fields.friends), 
+                                  builder: (context, AsyncSnapshot snapshot) {
+                                    if (snapshot.data == null) {
+                                      return const Center(child: CircularProgressIndicator());
+                                    } else {
+                                      return Text(
+                                        "${snapshot.data.fields.username}",
+                                        style: const TextStyle(
+                                            fontSize: 18.0,
+                                            fontWeight: FontWeight.bold,
+                                        ),
+                                      );
+                                    }
+                                  })
+                                ],
+                              ),
+                          ));
+                        }
+                    }
+                })
             ],
           )
         )
