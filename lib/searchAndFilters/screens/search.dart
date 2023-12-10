3dca92b17b5afea456f9f4126046b2f565c3d340
Rumintangjsi <rumintanghtgl@gmail.com>
2023-12-10 19:27:01 +0700

    selesai mengerjakan dropdown genre

--- lib/searchAndFilters/screens/search.dart
+++ lib/searchAndFilters/screens/search.dart
@@ -14,45 +14,41 @@ class BookResultsPage extends StatefulWidget {
   final String searchQuery;
   final String genre;
 
-  BookResultsPage({required this.searchQuery, this.genre = ''});
+  BookResultsPage({required this.searchQuery, this.genre = ' '});
 
   @override
   _BookResultsPageState createState() => _BookResultsPageState();
 }
 
 class _BookResultsPageState extends State<BookResultsPage> {
-  Future<List<Book>> fetchBooks(CookieRequest request, String searchQuery, String genre) async {
-  String url;
+  String genre = '';
 
-  if (searchQuery.isNotEmpty && genre.isEmpty){
-    url = 'http://127.0.0.1:8000/get_search_books/';
-  }
-  else{
-    url = 'http://127.0.0.1:8000/get_filtered_books/';
-  }
-  
-  if (searchQuery.isNotEmpty || genre.isNotEmpty) {
-    if (searchQuery.isNotEmpty) {
-      url += searchQuery ;
+  Future<List<Book>> fetchBooks(
+      CookieRequest request, String searchQuery, String genre) async {
+    String url = '';
+
+    if (searchQuery.isNotEmpty || genre.isNotEmpty) {
+      if (searchQuery.isNotEmpty) {
+        url = 'http://127.0.0.1:8000/get_search_books/' + searchQuery;
+      }
+      if (genre.isNotEmpty) {
+        url = 'http://127.0.0.1:8000/get_filtered_books/'+ genre;
+      }
+    } else {
+      throw Exception('Sorry the books you looking for is not found :(');
     }
-    if (genre.isNotEmpty) { 
-      url += genre ;
+
+    var response = await request.get(url);
+
+    List<Book> books = [];
+
+    for (var d in response) {
+      if (d != null) {
+        books.add(Book.fromJson(d));
+      }
     }
-  } else {
-    throw Exception('Sorry the books you looking for is not found :(');
-  }
-
-  var response = await request.get(url);
-
-  List<Book> books = [];
-
-  for (var d in response){
-    if (d != null) {
-      books.add(Book.fromJson(d));
-    }
-  }
-  // print(books);
-  return books;
+    // print(books);
+    return books;
   }
 
   @override
@@ -60,61 +56,56 @@ class _BookResultsPageState extends State<BookResultsPage> {
     final request = context.watch<CookieRequest>();
     return Scaffold(
       appBar: AppBar(title: Text('Search Results')),
-      body: FutureBuilder(
-            
-            future: fetchBooks(request, widget.searchQuery, widget.genre),
-            builder: (context, AsyncSnapshot snapshot) {
-                if (snapshot.data == null) {
-                    return const Center(child: CircularProgressIndicator());
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: DropdownMenuExample(
+              onGenreChanged: (newGenre) {
+                setState(() {
+                  genre = newGenre;
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: fetchBooks(request, widget.searchQuery, genre),
+              builder:
+                  (BuildContext context, AsyncSnapshot<List<Book>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
                 } else {
-                    if (!snapshot.hasData) {
-                    return const Column(
+                  return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (_, index) => Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 12),
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                        Text(
-                            "Tidak ada data Buku.",
-                            style:
-                                TextStyle(color: Color(0xff59A5D8), fontSize: 20),
-                        ),
-                        SizedBox(height: 8),
+                          Text(
+                            "${snapshot.data![index].fields.title}",
+                            style: const TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ],
-                    );
-                } else {
-                    return Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: DropdownMenuExample(),
-                              ),
-                              Expanded(
-                                child: ListView.builder(
-                                          itemCount: snapshot.data!.length,
-                                          itemBuilder: (_, index) => Container(
-                                                  margin: const EdgeInsets.symmetric(
-                                                      horizontal: 16, vertical: 12),
-                                                  padding: const EdgeInsets.all(20.0),
-                                                  child: Column(
-                                                  mainAxisAlignment: MainAxisAlignment.start,
-                                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                                  children: [
-                                                      Text(
-                                                      "${snapshot.data![index].fields.title}",
-                                                      style: const TextStyle(
-                                                          fontSize: 18.0,
-                                                          fontWeight: FontWeight.bold,
-                                                                  ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          );
-                    }
+                      ),
+                    ),
+                  );
                 }
-            }));
-    }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
-
-
--- lib/searchAndFilters/widgets/dropdownmenu.dart
+++ lib/searchAndFilters/widgets/dropdownmenu.dart
@@ -1,34 +1,25 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 import 'package:tenfoldlit_mobile/searchAndFilters/screens/search.dart';
 
-const List<String> list = <String>[
-  'Nonfiction', 'History', 'Games', 'Esoterica', 'Poetry', 'Cultural', 
-  'Religion', 'Romance', 'Prayer', 'History', 'Chess', 'Astrology', 
-  'Canada', 'Sequential', 'Theology', 'Christian', 'Evangelism', 
-  'Christianity', 'Science', 'Biography', 'Military'
-];
+const List<String> list = ['Nonfiction', 'History', 'Games', 'Esoterica', 'Poetry', 'Cultural', 'Religion', 'Romance', 
+'Prayer', 'History', 'Chess', 'Astrology', 'Canada', 'Sequential', 'Theology', 'Christian', '40k', 'Christianity', 
+'Science', 'Biography', 'Military', 'Tv', 'Classics', 'Entrepreneurship', 'Basketball', 'Combat', 'Storytime', 'Management', 
+'Economics', 'Contemporary Romance', 'Medieval History', 'Spirituality', 'Folklore', 'School', 'Religion', 'Brazil', 'Islam', 
+'Archaeology', 'Civil War', 'Sports', 'Kids', 'Germany', 'Money', 'Mythology', 'Culture', 'Games', 'Zen', 'M M M', 'Chick Lit', 
+'Art', 'Education', 'Erotica', 'Spy Thriller', 'Humor', 'Personal Finance', 'Christianity', 'Horses', 'Theology', 'Nazi Party', 
+'Mathematics', 'Memoir'];
 
-void main() => runApp(const DropdownMenuApp());
-
-class DropdownMenuApp extends StatelessWidget {
-  const DropdownMenuApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('DropdownMenu Sample')),
-        body: const Center(
-          child: DropdownMenuExample(),
-        ),
-      ),
-    );
-  }
-}
 
 class DropdownMenuExample extends StatefulWidget {
-  const DropdownMenuExample({Key? key}) : super(key: key);
+  final Function(String) onGenreChanged;
+
+  const DropdownMenuExample(
+      {required this.onGenreChanged, Key? key})
+      : super(key: key);
 
   @override
   State<DropdownMenuExample> createState() => _DropdownMenuExampleState();
@@ -37,38 +28,42 @@ class DropdownMenuExample extends StatefulWidget {
 class _DropdownMenuExampleState extends State<DropdownMenuExample> {
   String dropdownValue = list.first;
 
+  Future<void> updateBooks(String? newValue) async {
+    if (newValue != null) {
+      setState(() {
+        dropdownValue = newValue;
+      });
+      widget.onGenreChanged(dropdownValue);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      icon: Icon(Icons.arrow_downward),
-      iconSize: 24,
-      elevation: 16,
-      style: TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
+    return Container(
+      margin: EdgeInsets.only(left: 20.0),
+      padding: EdgeInsets.symmetric(horizontal: 4.0, vertical: 1.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5.0),
+        border: Border.all(color: Color.fromARGB(255, 148, 103, 48), width: 2),
+        color: Colors.white,
       ),
-      onChanged: (String? newValue) {
-        setState(() {
-          dropdownValue = newValue!;
-        });
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => BookResultsPage(
-              searchQuery: '',
-              genre: dropdownValue,
-            ),
-          ),
-        );
-      },
-      items: list.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<String>(
+          value: dropdownValue,
+          icon: Icon(Icons.keyboard_arrow_down, color: Color.fromARGB(255, 148, 103, 48)),
+          iconSize: 24,
+          elevation: 16,
+          style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
+          onChanged: updateBooks,
+          items: list.toSet().toList() // Remove duplicates
+              .map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
